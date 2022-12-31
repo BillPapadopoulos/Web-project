d3ab9104903ab7b6efc48c053088f6e10a17f112
petroskatsikarelis <65900062+petroskatsikarelis@users.noreply.github.com>
2022-12-31 11:34:01 +0200

    Add files via upload

--- Login Sign up.js
+++ Login Sign up.js
@@ -16,10 +16,21 @@ function clearInputError(inputElement) {
   inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
+function setMatchingError(inputElement, message){
+  inputElement.classList.add("form__input--error");
+  inputElement.parentElement.querySelector(".form__input-error-message2").textContent = message;
+}
+
+function clearMatchingError(inputElement) {
+  inputElement.classList.remove("form__input--error");
+  inputElement.parentElement.querySelector(".form__input-error-message2").textContent = "";
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
+  const password = document.getElementById("signupPassword");
+  const password2= document.getElementById("signupPassword2");
 
   document.querySelector("#linkCreateAccount").addEventListener("click", e => {
     e.preventDefault();
@@ -36,9 +47,17 @@ document.addEventListener("DOMContentLoaded", () => {
   loginForm.addEventListener("submit", e => {
     e.preventDefault();
 
-    
+    //AJAX OR NODE CONNECTION
 
     setFormMessage(loginForm, "error", "Invalid username/password combination");
+  });
+
+  createAccountForm.addEventListener("submit",e => {
+    e.preventDefault();
+
+    //AJAX OR NODE CONNECTION 
+
+    setFormMessage(createAccountForm, "error", "Cannot leave blanks");
   });
 
   document.querySelectorAll(".form__input").forEach(inputElement => {
@@ -47,10 +66,35 @@ document.addEventListener("DOMContentLoaded", () => {
         setInputError(inputElement, "Username must be at least 10 characters in length");
       }
     });
-    
+  });
+
+  document.querySelectorAll(".form__input").forEach(inputElement => {
+      inputElement.addEventListener("blur", e => {
+        if (e.target.id === "signupPassword" && e.target.value.length > 0 && e.target.value.length < 8) {
+          setInputError(inputElement, "Password must be at least 8 characters in length");
+        }  
+      });   
+
     inputElement.addEventListener("input", e => {
       clearInputError(inputElement);
     });
+  
   });
-});
 
+  document.querySelectorAll(".form__input2").forEach(inputElement => {
+    inputElement.addEventListener("blur", e => {
+      if (password.value != password2.value) {
+
+        setMatchingError(inputElement, "Passwords must be the same");
+        
+        }  
+      });
+
+    inputElement.addEventListener("input", e => {
+      clearMatchingError(inputElement);
+      });
+
+    });
+
+
+});
