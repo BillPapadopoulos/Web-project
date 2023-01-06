eebaf80ae84683e24f83cc2923b865463f55dee3
petroskatsikarelis <65900062+petroskatsikarelis@users.noreply.github.com>
2023-01-06 12:54:46 +0200

    Add files via upload

--- connect.php
+++ connect.php
@@ -4,22 +4,31 @@ $conn = mysqli_connect('localhost','root','', 'test');
 
 if(isset($_POST['register'])) {
 
-$username = $_POST['signupUsername'];
-$emailAddress = $_POST['email'];
-$password =  $_POST['signupPassword'];
+    $username = $_POST['signupUsername'];
+    $emailAddress = $_POST['email'];
+    $password =  $_POST['signupPassword'];
 
  //Database connection
 
-        $query = "INSERT INTO user (`user_username`, `email`, `password`) VALUES ('$username', '$emailAddress', '$password')";
-        $result = mysqli_query($conn,$query);
-       
-        if($result){
-            echo'<script> alert("Successful registration...")</script>';
+        $duplicate=mysqli_query($conn,"SELECT * FROM user WHERE user_username = '$username' OR email = '$emailAddress'");
+        if (mysqli_num_rows($duplicate)>0){
+            
+            echo'<script> alert("Username or Email already exists.")</script>';
+
         }
-        else {
-           echo'<script>alert("Unsuccessful registration...")</script>';
+        else{
+            $query = "INSERT INTO user (`user_username`, `email`, `password`) VALUES ('$username', '$emailAddress', '$password')";
+            $result = mysqli_query($conn,$query);    
+            if($result){
+                echo'<script> alert("Successful registration...")</script>';
+            }
+            else{
+                echo'<script>alert("Unsuccessful registration...")</script>';
+            }
+
         }
-    }
+
+}
 
 ?>
 
