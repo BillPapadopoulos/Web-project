2665ffefa57706c70f629c585a5b0605a0ae16b6
BillPapadopoulos <billaros218@gmail.com>
2023-08-18 13:31:08 +0300

    access to report offer from other oprtions

--- map/user_settings/change_credentials.php
+++ map/user_settings/change_credentials.php
@@ -55,7 +55,7 @@ src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js">
    <ul class="Starter Buttons">
      <li><a href="/web_database/map/map.php">Home</a></li>
      <li><a href=#>Search Offer by Category</a></li>
-     <li><a href=#>Report an Offer</a></li>
+     <li><a href="/web_database/map/report_offer/report_offer.php">Report an Offer</a></li>
      <li><a href="/web_database/map/show_offers/offers.php">Available Offers</a></li>
      <li class="pressed"><a href="settings.php">User : <?php echo $_SESSION['user_name']; ?> <br>Settings</a></li>
     </ul>
